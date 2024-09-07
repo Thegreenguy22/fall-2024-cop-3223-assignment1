ad4ac2321def143c25f98c41f048a5dd31dfc19b
Adam Sell <149280007+Thegreenguy22@users.noreply.github.com>
2024-09-07 03:26:50 +0000

    Worked on perimeter and area functions

--- fracturing.c
+++ fracturing.c
@@ -7,6 +7,8 @@
 #include <stdio.h>
 #include <math.h>
 
+#define PI 3.14159
+
 double calculateDistance(){
     double x1;
     double y1;
@@ -22,17 +24,27 @@ double calculateDistance(){
 
     distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 
-    printf("The distance is: %.2lf\n", distance);
-
     return distance;
 }
 
 double calculatePerimeter(){
+    double distance = calculateDistance();
 
+    double perimeter = (PI * distance);
+
+    printf("The perimeter of the city encompassed by your request is: %.2lf\n", perimeter);
+
+    return 3;
 }
 
 double calculateArea(){
+    double distance = calculateDistance();
 
+    double area = (0.25 * PI * pow(distance, 2));
+
+    printf("The area of the city encompassed by your request is: %.2lf\n", area);
+
+    return 3;
 }
 
 double calculateWidth(){
@@ -44,9 +56,11 @@ double calculateHeight(){
 }
 
 int main(int argc, char **argv){
-    calculateDistance();
-    // calculatePerimeter();
-    // calculateArea();
+    double distance = calculateDistance();
+    printf(" The distance between the two points is: %.2lf\n", distance);
+    
+    calculatePerimeter();
+    calculateArea();
     // calculateWidth();
     // calculateHeight();
     return 0;
